ceb7401af218a7d2e8fbc76d5e02aaf4849a5913
Bhavik Jain <bhavikjain403@gmail.com>
2022-04-06 20:59:18 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
       <Navbar dark color='primary'>
         <div className='container'>
           <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
-    </div>
-  );
+      <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  };
 }
 
 export default App;
