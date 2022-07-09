2af29f546e308417b88912bc29849a661c812e35
Bhavik Jain <bhavikjain403@gmail.com>
2022-07-09 18:09:00 +0530

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+
+function RenderCard({item}){
+    return(
+        <Card>
+            <CardImg src={item.image} alt={item.name}></CardImg>
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    )
+}
 
 export default function Home(props){
     return(
         <div className="container">
-            <h4>Home</h4>
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.dish}></RenderCard>
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.promotion}></RenderCard>
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.leader}></RenderCard>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -13,7 +17,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
@@ -24,7 +30,9 @@ class Main extends Component {
   render() {
     const HomePage = ()=>{
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+        promotion={this.state.promotions.filter((promotion)=>promotion.featured)[0]}
+        leader={this.state.leaders.filter((leader)=>leader.featured)[0]}/>
       )
     }
     return (
@@ -33,6 +41,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage}/>
           <Route exact path='/menu' component={()=> <Menu dishes={this.state.dishes}/>}/>
+          <Route exact path='/contactus' component={Contact}/>
           <Redirect to='/home'/>
         </Switch>
         <Footer/>
