7c1218944962909d62aae6221acbba4f44f6eec2
Bhavik Jain <bhavikjain403@gmail.com>
2022-07-10 16:21:52 +0530

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem    } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -51,27 +52,32 @@ function RenderComments({comments}){
         )
     }
     const DishDetail = (props)=>{
-        let d = props.dish
-        
-        if (d == null) {
-            return (<div></div>);
-        }
 
         // const dishItem = this.renderDish(this.props.dish);
         // const dishComment = this.renderComments(this.props.dish.comments);
-
+        if(props.dish!=null){
         return (
             <div className='container'>
                 <div className='row'>
-                    <div className='col-12 col-md-5 m-1'>
-                        <RenderDish dish={d}/>
+                <div className='row'>
+                    <Breadcrumb>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
                     </div>
-                    <div className='col-12 col-md-5 m-1'>
-                        <RenderComments comments={d.comments}/>   
+                </div>
+                    <div className='row'>
+                    <div className='col-12 col-md-6'><RenderDish dish={props.dish}/></div>
+                    <div className='col-12 col-md-6'><RenderComments comments={props.comments}/></div>
                     </div>
                 </div>
             </div>
-        )
+        )}
+        else
+        return(<div></div>)
     }
 
 export default DishDetail;
