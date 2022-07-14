00b8b4cacdc7bc516a9effb1a6bf1497c57c3c5e
Bhavik Jain <bhavikjain403@gmail.com>
2022-07-14 17:36:21 +0530

    Combining Reducers

---
+++ src/redux/dishes.js
@@ -0,0 +1,8 @@
+import { DISHES } from '../shared/dishes';
+
+export const Dishes = (state =  DISHES, action)=>{
+    switch (acton.type) {
+        default:
+            return state;
+    }
+}
---
+++ src/redux/promotions.js
@@ -0,0 +1,8 @@
+import { PROMOTIONS } from "../shared/promotions";
+
+export const Promotions = (state =  PROMOTIONS, action)=>{
+    switch (acton.type) {
+        default:
+            return state;
+    }
+}
